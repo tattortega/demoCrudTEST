e773ef04e12af1ee4976c998c574c603d8c13dc4
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-08 17:06:18 -0500

    Se agrega la clase que manejara las respuestas del API

---
+++ src/main/java/com/crud/democrud/utility/Response.java
@@ -0,0 +1,48 @@
+package com.crud.democrud.utility;
+
+/**
+ * Clase para el manejo de las respuestas de las API
+ *
+ * @version 1.0.0 2022-06-08
+ * @author Ricardo Ortega <tattortega.28@gmail.com>
+ * @since 1.0.0
+ */
+public class Response {
+
+    /**
+     * Indica de si existe un error o no en la respuesta del API
+     */
+    public Boolean error;
+    /**
+     * Mensaje del API cuando es utilizada
+     */
+    public String message;
+    /**
+     * Información del API cuando es necesario
+     */
+    public Object data;
+
+    /**
+     * Constructor de la clase
+     *
+     * @author Ricardo Ortega <tattortega.28@gmail.com>
+     * @since 1.0.0
+     */
+    public Response() {
+        error = false;
+        message = "";
+        data = null;
+    }
+
+    /**
+     * Restaura por defecto la respuesta del API
+     *
+     * @author Ricardo Ortega <tattortega.28@gmail.com>
+     * @since 1.0.0
+     */
+    public void restart() {
+        error = false;
+        message = "";
+        data = null;
+    }
+}
