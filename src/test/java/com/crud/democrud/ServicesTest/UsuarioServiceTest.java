d3a69954ac09b9946dd9d82390d0b0ffe6d2712d
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-08 20:12:12 -0500

    Se agrega la documentacion

--- src/test/java/com/crud/democrud/ServicesTest/UsuarioServiceTest.java
+++ src/test/java/com/crud/democrud/ServicesTest/UsuarioServiceTest.java
@@ -2,7 +2,6 @@ package com.crud.democrud.ServicesTest;
 
 import com.crud.democrud.models.UsuarioModel;
 import com.crud.democrud.repositories.UsuarioRepository;
-import com.crud.democrud.services.UsuarioService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,32 +10,38 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.List;
 import java.util.Optional;
 
-import static  org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test Data JPA Usuario
+ *
+ * @author Edgar Morillo <edgar.morillo@sofka.com.co>
+ * @version 1.0.0 2022-06-08
+ */
 @DataJpaTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
-public class UsuarioServiceTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UsuarioServiceTest {
     @Autowired
     UsuarioRepository usuarioRepository;
 
     @Test
-    public void testGuardarUsuario(){
-        UsuarioModel usuarioModel=new UsuarioModel("aquaman","aqua@gmail.com",99);
+    void testSaveUser() {
+        UsuarioModel usuarioModel = new UsuarioModel("mateo", "mateo@gmail.com", 70);
         UsuarioModel usuarioModelRegistrado = usuarioRepository.save(usuarioModel);
         assertNotNull(usuarioModelRegistrado);
     }
 
     @Test
-    public void testBuscarUsuarioPorId(){
-        Long idBuscado=1L;
-        Optional<UsuarioModel> usuarioModelBuscado=usuarioRepository.findById(idBuscado);
+    void testFindUserById() {
+        Long idBuscado = 1L;
+        Optional<UsuarioModel> usuarioModelBuscado = usuarioRepository.findById(idBuscado);
         assertThat(usuarioModelBuscado.get().getId()).isEqualTo(idBuscado);
     }
 
     @Test
-    public void testListarUsuarios(){
-        List<UsuarioModel> usuarioModelList=(List<UsuarioModel>) usuarioRepository.findAll();
+    void testGetAllUsers() {
+        List<UsuarioModel> usuarioModelList = (List<UsuarioModel>) usuarioRepository.findAll();
         assertThat(usuarioModelList).size().isGreaterThan(0);
     }
 }
