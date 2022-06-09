48624d35bb381d87f9cc7e697ab68419ce799cd9
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-08 20:11:21 -0500

    Se agrega la documentacion

--- src/test/java/com/crud/democrud/ControllerServiceTest/UsuarioServiceMockTest.java
+++ src/test/java/com/crud/democrud/ControllerServiceTest/UsuarioServiceMockTest.java
@@ -1,44 +1,39 @@
 package com.crud.democrud.ControllerServiceTest;
 
-import com.crud.democrud.controllers.UsuarioController;
-import com.crud.democrud.models.UsuarioModel;
 import com.crud.democrud.repositories.UsuarioRepository;
 import com.crud.democrud.services.UsuarioService;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
+/**
+ * Test Mock Usuario
+ *
+ * @author Edgar Morillo <edgar.morillo@sofka.com.co>
+ * @version 1.0.0 2022-06-08
+ * @since 1.0.0
+ */
 @SpringBootTest
-public class UsuarioServiceMockTest {
+class UsuarioServiceMockTest {
 
-  @MockBean
-  UsuarioRepository usuarioRepository;
+    @MockBean
+    UsuarioRepository usuarioRepository;
 
-  @Autowired
-  UsuarioService usuarioService;
+    @Autowired
+    UsuarioService usuarioService;
 
-  @Test
-  public void testUsuarioMock(){
-    when(usuarioRepository.findAll()).thenReturn(new ArrayList<>());
-    assertThat(usuarioService.obtenerUsuarios()).isEmpty();
-    verify(usuarioRepository).findAll();
-  }
+    @Test
+    void testUsuarioMock() {
+        when(usuarioRepository.findAll()).thenReturn(new ArrayList<>());
+        assertThat(usuarioService.getUsers()).isEmpty();
+        verify(usuarioRepository).findAll();
+    }
 
 }
