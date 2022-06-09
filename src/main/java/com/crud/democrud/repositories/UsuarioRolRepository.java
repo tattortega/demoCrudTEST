651026098c6d5b42971553704883aa974c044a15
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-08 20:05:00 -0500

    Se agrega documentacion

--- src/main/java/com/crud/democrud/repositories/UsuarioRolRepository.java
+++ src/main/java/com/crud/democrud/repositories/UsuarioRolRepository.java
@@ -5,8 +5,21 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+/**
+ * Interfaz del repositorio de Entidad del Rol de Usuario
+ *
+ * @author Ricardo Ortega <tattortega.28@gmail.com>
+ * @version 1.0.0 2022-06-08
+ * @since 1.0.0
+ */
 public interface UsuarioRolRepository extends CrudRepository<UsuarioRolModel, Long> {
 
-    public abstract Optional<UsuarioRolModel> findByRol(String rol);
+    /**
+     * Método para buscar por nombre de rol
+     *
+     * @param rol String
+     * @return Optional
+     */
+    Optional<UsuarioRolModel> findByRol(String rol);
 
 }
